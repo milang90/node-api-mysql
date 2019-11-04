a7fd3d397d03174f1451884426d56bfd18ddd836
Milan Gotera <milangotera@gmail.com>
2019-11-04 18:29:49 -0500

    Permitiendo la consulta de cualquier origen

--- app/index.js
+++ app/index.js
@@ -11,6 +11,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const router = require('./routes');
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use('/api', router);
 
 app.listen(PORT, () => {
